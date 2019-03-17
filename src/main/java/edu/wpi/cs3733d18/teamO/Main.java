9cec635135b1633ebddd97defdacc9e9f8844312
Yangjack <yyang17@wpi.edu>
2019-03-17 14:08:58 -0400

    Added name for Yicheng Yang

--- src/main/java/edu/wpi/cs3733d18/teamO/Main.java
+++ src/main/java/edu/wpi/cs3733d18/teamO/Main.java
@@ -8,6 +8,7 @@ public final class Main {
 
   public static void main(String... args) {
       System.out.println("Austin");
+      System.out.println("Jack Yang");
       System.out.println("Hello, World");
   }
 }
