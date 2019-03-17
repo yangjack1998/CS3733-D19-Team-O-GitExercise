19388ee44787da398616ec79730f263185be4cd3
Austin Shalit <austinshalit@gmail.com>
2019-03-17 14:04:21 -0400

    Austin

---
+++ src/main/java/edu/wpi/cs3733d18/teamO/Main.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs3733d18.teamO;
+
+public final class Main {
+
+  private Main() {
+
+  }
+
+  public static void main(String... args) {
+      System.out.println("Austin");
+      System.out.println("Hello, World");
+  }
+}
